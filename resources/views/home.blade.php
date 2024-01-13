72524f3d9a567b4c6c6ba82f8d8ad99a2cc82412
vanVulpenPaul <thebaldcoder@gmail.com>
2024-01-13 17:06:01 +0100

    Updated the README file.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.base')
 
 @section('content')
-    <h1>Welcome page</h1>
+<h1 class="mb-3">Welcome page</h1>
+<p>Go to the Posts to watch the CRUD implementation of this application.</p>
 @endsection
